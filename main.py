cd8c9e9bfb39f3bb7c23e9906eb434d248c456f8
gorrego <gorrego@bancolombia.com.co>
2024-11-26 21:46:48 -0500

    adicionando adjetivos

--- main.py
+++ main.py
@@ -7,33 +7,33 @@ app = FastAPI()
 # Diccionario de categorías con palabras clave y respuestas
 categorias = {
     "saludo": {
-        "palabras_claves": ["hola", "buenos dias", "buenas tardes", "buenas noches"],
+        "palabras_claves": ["hola", "buenos dias", "buenas tardes", "buenas noches", "que tal", "como estas", "saludos"],	
         "respuestas": ["Hola, ¿qué tal?", "Buenos días, un gusto saludarte", "Buenas tardes, un gusto saludarte", "Buenas noches, que descanses"]
     },
     "despedida": {
-        "palabras_claves": ["adios", "chao", "hasta luego", "nos vemos", "bye"],
+        "palabras_claves": ["adios", "chao", "hasta luego", "nos vemos", "bye", "gracias","suerte"],
         "respuestas": ["Gracias por su visita", "Un gusto atenderlo", "Que tenga un buen día", "Nos vemos pronto"]
     },
     "feliz": {
-        "palabras_claves": ["me siento feliz", "estoy muy contento", "estoy alegre", "me siento de maravilla", "todo va bien"],
+        "palabras_claves": ["me siento feliz", "estoy muy contento", "estoy alegre", "me siento de maravilla", "todo va bien", "feliz","dichoso", "contento", "satisfecho", "alegre", "boyante", "próspero", "afortunado", "ufano", "radiante", "bienaventurado"],
         "respuestas": ["¡Eso es genial! Me alegra escuchar eso.", "¡Qué bien! ¿Algo especial que haya pasado?", "¡Maravilloso! Cuéntame más sobre tu día.", "¡Genial! ¿Te gustaría compartir qué te hace sentir tan bien?"]
     },
     "triste":{
-        "palabras_claves": ["me siento triste", "estoy deprimido", "no me siento bien", "me siento desanimado", "estoy muy mal"],
+        "palabras_claves": ["me siento triste", "estoy deprimido", "no me siento bien", "me siento desanimado", "estoy muy mal","triste","afligido", "apenado", "entristecido", "apesadumbrado", "atribulado", "pesaroso", "mohíno", "mustio", "taciturno", "compungido", "lloroso", "cariacontecido","infausto", "funesto", "deplorable", "lamentable", "luctuoso", "tétrico", "lúgubre", "trágico", "aciago"],
         "respuestas": ["Lamento escuchar eso. ¿Quieres hablar al respecto?", "Siento que te sientas así. Estoy aquí para escucharte.", "Es normal sentirse triste a veces. ¿Hay algo que pueda hacer para ayudarte?", "Lo siento mucho. ¿Te gustaría compartir lo que te está pasando?"]
     },
      "enojado":{
-        "palabras_claves": ["estoy enojado", "me siento furioso", "estoy muy molesto", "tengo mucha rabia", "me siento frustrado"],
+        "palabras_claves": ["estoy enojado", "me siento furioso", "estoy muy molesto", "tengo mucha rabia", "me siento frustrado","enojado","airado" ,"alterado" , "áspero", "bravo", "cabreado" ,"carilargo" ,"colérico" ,"enfadado" ,"irritado" ,"mohíno", "molesto" ,"picado" ,"resentido"],
         "respuestas": ["Lamento que te sientas así. ¿Quieres hablar de lo que te hace sentir enojado?", "La rabia puede ser difícil de manejar. Estoy aquí para escucharte.", "Siento que estés pasando por esto. ¿Hay algo que pueda hacer para ayudarte?", "Es comprensible sentirse frustrado. Cuéntame más sobre lo que está pasando."]
     },
     "ansioso":{
-        "palabras_claves": ["me siento ansioso", "estoy nervioso", "tengo mucha ansiedad", "me siento inquieto", "estoy preocupado"],
+        "palabras_claves": ["me siento ansioso", "estoy nervioso", "tengo mucha ansiedad", "me siento inquieto", "estoy preocupado","ansioso","nervioso"],
         "respuestas": ["Lo siento. La ansiedad puede ser abrumadora. ¿Hay algo específico que te preocupe?", "Entiendo que te sientas ansioso. ¿Quieres hablar de lo que te está causando ansiedad?", "Estoy aquí para ti. Cuéntame más sobre cómo te sientes.", "La preocupación puede ser difícil de manejar. ¿Hay algo que pueda hacer para ayudarte?"]
     },
     "estresado":{
-        "palabras_claves": ["me siento estresado", "estoy bajo mucho estrés", "tengo mucho estrés", "me siento abrumado", "estoy muy estresado"],
+        "palabras_claves": ["me siento estresado", "estoy bajo mucho estrés", "tengo mucho estrés", "me siento abrumado", "estoy muy estresado","estresado"],
         "respuestas": ["Siento que estés estresado. ¿Quieres hablar de lo que te está causando estrés?", "El estrés puede ser muy agotador. Estoy aquí para ayudarte.", "Lamento que te sientas abrumado. ¿Hay algo específico que te cause estrés?", "Estoy aquí para escucharte. Cuéntame más sobre lo que te está pasando."]
-    },
+    }
     
 }
 
